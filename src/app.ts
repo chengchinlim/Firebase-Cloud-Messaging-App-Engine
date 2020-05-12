7e25701cc5ba49a6fd261c078a4a3edd35e1f474
chengchinlim <chengchinlim@Chengs-MacBook-Pro.local>
2020-05-12 16:51:25 -0700

    Add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 const express = require('express');
+const axios = require('axios')
+const cors = require('cors')
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({ origin: true }))
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+app.get('/', (req, res, next) => {
+  if (req.query.switch === 'stop') {
+    res.status(200).send('Blocked!').end()
+    return 
+  }
+  next()
+},(req, res) => {
+    res.status(200).send('Hello from GCP app engine Cloud Messaging!').end()
 });
 
 // Start the server
