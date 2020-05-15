910139006a0bf8b6729cad7e88d39c2bdbd9be81
chengchinlim <chengchinlim@Chengs-MacBook-Pro.local>
2020-05-15 15:36:36 -0700

    Learnt bunyan

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,61 @@
-const express = require('express');
+const express = require('express')
 const axios = require('axios')
 const cors = require('cors')
+// const dotenv = require('dotenv')
+const bunyan = require('bunyan')
+const lb = require('@google-cloud/logging-bunyan')
 
-const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: true }))
+export const GOOGLE_APPLICATION_CREDENTIALS='/Users/chengchinlim/BackendProjects/firebase-cloud-messaging-service-account-key.json'
 
-app.get('/', (req, res, next) => {
-  if (req.query.switch === 'stop') {
-    res.status(200).send('Blocked!').end()
-    return 
-  }
-  next()
-},(req, res) => {
-    res.status(200).send('Hello from GCP app engine Cloud Messaging!').end()
-});
+const startServer = async () => {
+    const { logger, mw } = await lb.express.middleware()
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+    const app = express();
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use(cors({ origin: true }))
+    app.use(mw)
 
-module.exports = app;
+// const loggingBunyan = new lb.LoggingBunyan()
+
+// const logger = bunyan.createLogger({
+//   name: 'my-service',
+//   streams: [
+//     { level: 'info', stream: process.stdout },
+//     loggingBunyan.stream('info')
+//   ]
+// })
+
+    app.get('/', (req, res, next) => {
+      if (req.query.switch === 'stop') {
+        // logger.error({
+        //   labels: {
+        //     issue: 'ooops',
+        //     solution: 'should be ok'
+        //   } 
+        // }, 'Testing info logging')
+        req.log.error({
+          labels: {
+            issue: 'ooops',
+            solution: 'should be ok'
+          }
+        }, 'Error message')
+        res.status(200).send('Blocked!').end()
+        return 
+      }
+      next()
+    },(req, res) => {
+        logger.error('Testing error logging')
+        res.status(200).send('Hello from GCP app engine Cloud Messaging!').end()
+    });
+
+    // Start the server
+    const PORT = process.env.PORT || 8888;
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+      console.log('Press Ctrl+C to quit.');
+    });
+
+}
+
+startServer()
