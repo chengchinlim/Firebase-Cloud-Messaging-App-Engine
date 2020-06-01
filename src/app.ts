01d5dc7cfb3364faf76aaff5be8f95ab78257d53
chengchinlim <chengchinlim@Chengs-MacBook-Pro.local>
2020-05-31 17:33:25 -0700

    Setup master

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,31 @@
 const express = require('express')
-// const axios = require('axios')
+const axios = require('axios')
 const cors = require('cors')
-// const dotenv = require('dotenv')
-// const bunyan = require('bunyan')
-// const lb = require('@google-cloud/logging-bunyan')
 
-export const GOOGLE_APPLICATION_CREDENTIALS='/Users/chengchinlim/BackendProjects/firebase-cloud-messaging-service-account-key.json'
+// export const GOOGLE_APPLICATION_CREDENTIALS='/Users/chengchinlim/BackendProjects/firebase-cloud-messaging-service-account-key.json'
 
 const startServer = async () => {
-    // const { logger, mw } = await lb.express.middleware()
 
     const app = express();
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(cors({ origin: true }))
-    // app.use(mw)
-
-// const loggingBunyan = new lb.LoggingBunyan()
-
-// const logger = bunyan.createLogger({
-//   name: 'my-service',
-//   streams: [
-//     { level: 'info', stream: process.stdout },
-//     loggingBunyan.stream('info')
-//   ]
-// })
 
     app.get('/', (req, res, next) => {
       if (req.query.switch === 'stop') {
-        // logger.error({
-        //   labels: {
-        //     issue: 'ooops',
-        //     solution: 'should be ok'
-        //   } 
-        // }, 'Testing info logging')
-        res.write('First response\n')
-        res.write('Waiting\n')
-        setTimeout(() => {
-            res.write('Second response\n')
-            res.end()
-        }, 1000)
-        return 
+
+        res.write('Hello\n');
+        setInterval(function() {
+          res.end('World\n');
+        },1000);
+
+        // axios.get('https://jsonplaceholder.typicode.com/posts').then(result => {
+        //     res.status(200).send(result.data)
+        // })
+        return
       }
       next()
     },(req, res) => {
-        // logger.error('Testing error logging')
-
-
         res.status(200).send('Hello from GCP app engine Cloud Messaging!').end()
     });
 
