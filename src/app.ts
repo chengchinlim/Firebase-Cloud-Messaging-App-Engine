7d6b3af6e8a5f783e81efa89e46dcf34694cb419
chengchinlim <chengchin5227@gmail.com>
2020-09-04 12:02:43 -0700

    Connect two Firestore with one app engine

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 const express = require('express')
 const axios = require('axios')
 const cors = require('cors')
-require('custom-env').env('test')
+const admin = require('firebase-admin')
 
-// export const GOOGLE_APPLICATION_CREDENTIALS='/Users/chengchinlim/BackendProjects/firebase-cloud-messaging-service-account-key.json'
+require('custom-env').env('test')
 
 const app = express();
 app.use(express.json())
@@ -12,8 +12,55 @@ app.use(cors({ origin: true }))
 
 app.get('/',(req, res) => {
     console.log(`Stripe live key: ${process.env.STRIPE_TEST_KEY}`)
-    res.status(200).send('Hello from GCP app engine Cloud Messaging! (Dev)').end()
+    res.status(200).send('Hello from GCP app engine Cloud Messaging! (Prod)').end()
 });
+
+const defaultApp = admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://cloudmessaging-5227.firebaseio.com'
+})
+
+const otherAppServiceAccountPath = '/Users/chengchinlim/Backend/cloud-functions-5227-key.json'
+const otherApp = admin.initializeApp({
+    credential: admin.credential.cert(otherAppServiceAccountPath),
+    databaseURL: 'https://cloud-functions-5227.firebaseio.com'
+}, 'other')
+
+app.get('/default', (req, res) => {
+    const db = defaultApp.firestore()
+    db.collection('ids')
+        .doc('test')
+        .get()
+        .then(doc => {
+            const data = doc.data()
+            if (data) {
+                console.log(`test: ${data['test']}`)
+                res.status(200).send('Success')
+            } else {
+                res.status(400).send('Failed')
+            }
+        }).catch(err => {
+            res.status(400).send(err)
+        })
+})
+
+app.get('/other', (req, res) => {
+    const db = otherApp.firestore()
+    db.collection('fruits')
+        .doc('N4h0ubC6CuRgGFmqbbAI')
+        .get()
+        .then(doc => {
+            const data = doc.data()
+            if (data) {
+                console.log(`name: ${data['name']}`)
+                res.status(200).send('Success')
+            } else {
+                res.status(400).send('Failed')
+            }
+        }).catch(err => {
+            res.status(400).send(err)
+        })
+})
 
 // Start the server
 const PORT = process.env.PORT || 8888;
